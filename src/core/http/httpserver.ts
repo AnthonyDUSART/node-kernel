a4f1579b0bd8359eb5be031c7330a4e99923936a
Anthony DUSART <anthony.dusart.pro@gmail.com>
2021-03-29 00:11:31 +0200

    Too much POST data fix

--- src/core/http/httpserver.ts
+++ src/core/http/httpserver.ts
@@ -32,6 +32,11 @@ export default class HTTPServer {
         incomingMessage.on('data', chunk =>
         {
             body += chunk;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                incomingMessage.socket.destroy();
         });
 
         incomingMessage.on('end', () =>
